f52631b3ac764a549894b0f70ed1166ae46d2966
Muhammad Ali Raza <alirazaitservice@gmail.com>
2026-03-09 09:09:07 +0500

    Navbar & Hero Section Complete

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -1,7 +1,49 @@
 export default function Hero() {
-    return (
-        <div className="max-w-[1160px] mx-auto  h-[60vh] bg-[#416BCD] rounded-[20px]">
+  return (
+    <div className="max-w-[1180px] mx-auto h-[60vh] px-4">
 
+      <div className="flex items-center justify-between h-full bg-gradient-to-r from-blue-950 via-blue-700 to-blue-500 rounded-[20px] p-8 gap-10">
+        
+        {/* Text Section */}
+        <div>
+          <h1 className="text-white lg:text-[55px] md:text-[30px] font-medium text-[27px]">
+            Elevate Your Audio Journey
+          </h1>
+
+          <div className="lg:max-w-[600px] md:max-w-[320px] md:text-[14px]">
+            <p className="text-amber-50 lg:text-[16px] md:text-[15] mt-1 text-[13px]">
+              Experience Sound in its Purest Form. Morbi justo sem, venenatis
+              sit amet tortor id, porttitor facilisis metus. Ut scelerisque
+              mauris vivamus fringilla elit eu felis cursus.
+            </p>
+
+            {/* Buttons */}
+            <div className="mt-[40px] flex gap-8">
+              <a
+                href="/"
+                className="bg-[#416BCD] py-[10px] px-[18px] rounded-[10px] text-amber-50 font-medium"
+              >
+                Shop Now
+              </a>
+
+              <a
+                href="/"
+                className="border border-white py-[10px] px-[18px] rounded-[10px] text-amber-50 font-medium"
+              >
+                Learn More
+              </a>
+            </div>
+          </div>
         </div>
-    )
+
+        {/* Image */}
+        <img
+          src="/images/hero.png"
+          alt="Hero"
+          className="object-cover h-full sm:block hidden"
+        />
+        
+      </div>
+    </div>
+  );
 }
--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -3,34 +3,34 @@ import { HiOutlineShoppingCart } from "react-icons/hi";
 
 export default function NavBar() {
   return (
-    <div className="max-w-[1200px] mx-auto px-4 mb-">
+    <div className="max-w-[1200px] mx-auto px-4 mb-[20px]">
       <div className="py-4">
         <nav className="flex items-center justify-between">
 
           {/* Logo */}
-          <a href="/" className="text-3xl flex items-center gap-1">
+          <a href="/" className="text-3xl flex items-center gap-1 hover:text-[#416BCD]">
             <LiaSignalSolid />
-            <p className="text-[22px] font-[500] text-[#282A32]">
+            <p className="text-[22px] font-[500] text-[#282A32] hover:text-[#416BCD]">
               CRESCENDO
             </p>
           </a>
 
           {/* Menu */}
           <ul className="hidden lg:flex gap-6">
-            <li><a href="/" className="font-[500]">Home</a></li>
-            <li><a href="/products" className="font-[500]">Products</a></li>
-            <li><a href="/about" className="font-[500]">About</a></li>
-            <li><a href="/find" className="font-[500]">Find Us</a></li>
-            <li><a href="/pages" className="font-[500]">Pages</a></li>
+            <li><a href="/" className="font-[500] hover:text-[#416BCD]">Home</a></li>
+            <li><a href="/products" className="font-[500] hover:text-[#416BCD]">Products</a></li>
+            <li><a href="/about" className="font-[500] hover:text-[#416BCD]">About</a></li>
+            <li><a href="/find" className="font-[500] hover:text-[#416BCD]">Find Us</a></li>
+            <li><a href="/pages" className="font-[500] hover:text-[#416BCD]">Pages</a></li>
           </ul>
 
           {/* Right side */}
           <div className="flex items-center gap-4">
             <a href="/cart">
-              <HiOutlineShoppingCart className="text-[22px]" />
+              <HiOutlineShoppingCart className="text-[22px] hover:text-[#416BCD]"/>
             </a>
 
-            <button className="hidden sm:block bg-[#416BCD] text-white py-2 px-4 rounded-lg font-[500]">
+            <button className="hidden sm:block cursor-pointer bg-[#416BCD] text-white py-2 px-4 rounded-lg font-[500] hover:bg-[#3355A0]">
               Buy Template
             </button>
           </div>
